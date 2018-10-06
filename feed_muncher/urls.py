137aa231c03a345db48ab2c81c9243c92845a372
Daniel Rozenberg <me@danielrozenberg.com>
2018-10-05 21:33:58 -0400

    Updated requirements.txt and fixed deprecated features

--- feed_muncher/urls.py
+++ feed_muncher/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('login/', auth_views.login, kwargs={'template_name': 'login.html'}, name='login'),
-    path('logout/', auth_views.logout, kwargs={'next_page': '/'}, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
     path('feed/', include('app.urls')),
     path('', RedirectView.as_view(permanent=False, pattern_name='list_feeds')),
